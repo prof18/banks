89b52cc32bfaf468fc4620410d3ec9b7579f7fd4
marco <badprof18@gmail.com>
2016-07-06 12:35:34 +0200

    First Commit

---
+++ src/com/digifarm/BANKS.java
@@ -0,0 +1,28 @@
+package com.digifarm;
+
+/**
+ * Created by marco on 7/6/16.
+ */
+/*
+	BANKS implementation.
+
+	TO DO:
+
+		0) implementazione della classe TupleNode
+			- lista con i nodi adiacenti
+			- nome della tabella e chiave primaria
+
+		1) implementazione della lettura dei dati dal database e costruzione dei nodi
+			- costruisco il grafo popolando ogni TupleNode con: adiacenze e chiave primaria
+
+		2) implementazione della navigazione con Dijkstra
+
+*/
+
+public class BANKS {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}

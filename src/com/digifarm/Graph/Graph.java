951399cf63c936f823bf5a0ba30d8e8a401de94f
marco <badprof18@gmail.com>
2016-08-02 19:15:58 +0200

    Added Graph class

--- src/com/digifarm/Graph/Graph.java
+++ src/com/digifarm/Graph/Graph.java
@@ -1,7 +1,32 @@
 package com.digifarm.Graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by marco on 7/7/16.
  */
 public class Graph {
+
+    HashMap<Integer, Node> nodeSet;
+    ArrayList<Edge> edge = new ArrayList<>();
+    ArrayList<Edge> bedge = new ArrayList<>();
+
+    public Graph(HashMap<Integer, Node> nodeSet, ArrayList<Edge> edge, ArrayList<Edge> bedge) {
+        this.nodeSet = nodeSet;
+        this.edge = edge;
+        this.bedge = bedge;
+    }
+
+    public HashMap<Integer, Node> getNodeSet() {
+        return nodeSet;
+    }
+
+    public ArrayList<Edge> getEdge() {
+        return edge;
+    }
+
+    public ArrayList<Edge> getBedge() {
+        return bedge;
+    }
 }

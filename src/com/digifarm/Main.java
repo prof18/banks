3f61ed7d4e3eb68785b92e5832fea2479c1a37a4
marco <badprof18@gmail.com>
2016-08-09 18:30:12 +0200

    press silence

--- src/com/digifarm/Main.java
+++ src/com/digifarm/Main.java
@@ -12,21 +12,7 @@ import java.util.Scanner;
 /**
  * Created by marco on 7/6/16.
  */
-/*
-	Banks implementation.
 
-	TO DO:
-
-		0) implementazione della classe TupleNode
-			- lista con i nodi adiacenti
-			- nome della tabella e chiave primaria
-
-		1) implementazione della lettura dei dati dal database e costruzione dei nodi
-			- costruisco il grafo popolando ogni TupleNode con: adiacenze e chiave primaria
-
-		2) implementazione della navigazione con Dijkstra
-
-*/
 
 public class Main {
     public static void main(String[] args) {

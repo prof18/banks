b987f6f62d536f446e4ec537a4a46df000a5d644
marco <marco.gomiero.93@gmail.com>
2016-09-13 23:34:40 +0200

    working on main ui

--- src/com/digifarm/UI/MainUI.java
+++ src/com/digifarm/UI/MainUI.java
@@ -4,6 +4,9 @@ import com.digifarm.DBConnection.ConnectionDB;
 import com.digifarm.Exec.Main;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,22 +28,25 @@ public class MainUI extends JFrame implements ActionListener, WindowListener {
 
     private JTextArea textArea = new JTextArea();
     private JTextField keyword;
-    private JPanel first;
+    private JPanel first, inside;
     private JButton search;
-    private JLabel title1;
+    private JLabel title1 = new JLabel("Enter keyword. Use space as separator");
+    private JScrollPane console;
+
+    private Border blackline =  BorderFactory.createLineBorder(Color.black);
 
 
     public MainUI() {
         //configurazione generale del JFrame
         setTitle("Banks by Digifarmer");
-        setSize(950, 750);
-        setResizable(false);
+        setSize(950, 350);
+     //   setResizable(false);
         setVisible(false);
         //jframe centering
         Dimension dim = getToolkit().getScreenSize();
-        setLocation((dim.width - getWidth()) / 2, (dim.height - getHeight()) / 2);
+        //setLocation((dim.width - getWidth()) / 2, (dim.height - getHeight()) / 2);
         //layout configuration
-        setLayout(new GridLayout(2, 1));
+      //  setLayout(new GridLayout(0, 1));
         //end program when click on X
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -51,16 +57,63 @@ public class MainUI extends JFrame implements ActionListener, WindowListener {
         System.setOut(printStream);
         System.setErr(printStream);
 
-        first = new JPanel(new GridLayout(4,1));
-        title1 = new JLabel("Enter keyword. (Space as separator");
+      //first = new JPanel(new GridLayout(7,1));
+     //   first.setBorder(new EmptyBorder(0, 20, 20, 20));
+     /* //  first.setBorder(blackline);
+        inside = new JPanel(new GridLayout(1,2,100,0));
+
+        title1 = new JLabel("Enter keyword. Use space as separator");
+        title1.setHorizontalAlignment(JLabel.CENTER);
+
+        //keyword.setPreferredSize(new Dimension(150, 80));
+        */
         keyword = new JTextField();
-        keyword.setToolTipText("Enter keyword. (Space as separator");
         search = new JButton("Search");
-        first.add(title1);
-        first.add(keyword);
-        first.add(search);
-        add(first);
-        add(new JScrollPane(textArea));
+
+        // creates the GUI
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+       // add(title1,constraints);
+
+        constraints.gridx = 1;
+        constraints.fill=GridBagConstraints.HORIZONTAL;
+        constraints.insets = new Insets(20, 20, 20, 20);
+        add(keyword, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(20, 20, 20, 20);
+        constraints.anchor = GridBagConstraints.EAST;
+
+        add(search, constraints);
+
+
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weightx = 1.0;
+        constraints.weighty = 1.0;
+
+
+
+
+
+        console = new JScrollPane(textArea);
+        add(console, constraints);
+        textArea.setBorder(new EmptyBorder(20,20,20,20));
+        textArea.setEditable(false);
+        //textArea.setBorder(BorderFactory.createLineBorder(Color.black));
+       /* first.add(title1);
+        inside.add(keyword);
+        inside.add(search, BorderLayout.EAST);
+        first.add(inside);*/
+   //     add(first);
+
+       // add(console);
+
         search.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
